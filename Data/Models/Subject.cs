5ae2633b8138934b42f9bfc40a48a58a385eb9f2
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-02 19:15:41 +0200

    add user , subject models

---
+++ Data/Models/Subject.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace API_PRO.Data.Models
+{
+    public class Subject
+    {
+        [Key]
+        public int SubjectId { get; set; }
+
+        [Required]
+        public required string SubjectName { get; set; }
+
+        public int NumExams { get; set; } = 0;
+
+        public int TotalQuestions { get; set; } = 0;
+
+        //public required int UserId { get; set; }
+
+        //public ICollection<Exam> Exams { get; set; } = new List<Exam>();
+
+        //[Required]
+        //public required User User { get; set; }
+    }
+}
---
+++ Data/Models/Users.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace API_PRO.Data.Models
+{
+    public class Users : IdentityUser
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        public  string UserName { get; set; }
+        [Required]
+        public  string Password { get; set; }
+
+
+
+    }
+
+}
