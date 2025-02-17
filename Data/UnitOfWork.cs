7dc77a1439755a8bd7160edbaf076969360b3b7c
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-17 20:31:30 +0200

    add Unitofwork class

---
+++ Data/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API_PRO.Data
+{
+    public class UnitOfWork<T> : IUnitOfWork  where T : class
+    {
+        private readonly AppDbContext _db;
+        public IDataRepository<T> repo { get; private set; }
+        public UnitOfWork (AppDbContext db)
+        {
+            _db = db;
+            repo = new DataRepository<T>(db);
+        }
+        public int CommitChanges()
+        {
+            return _db.SaveChanges();  
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+    }
+}
