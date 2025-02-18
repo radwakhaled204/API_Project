4d81dd3716eaf37b004c572ca1e1a6de8914ab1e
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-18 22:29:55 +0200

    add relationship with order item table

--- Data/Models/Order.cs
+++ Data/Models/Order.cs
@@ -10,8 +10,10 @@ namespace API_PRO.Data.Models
         [Key]
         public int id { get; set; }
         public DateTime CreatedDate { set; get; }
+        
         [JsonIgnore]
         [IgnoreDataMember]
+        //many to many relationship order with item table
         public virtual ICollection<OrderItem>? ordersItems { get; set; }
     }
 }
