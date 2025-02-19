413e6f4f03d9a9d54e227d1cae6a3da828f97aa1
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-19 21:13:36 +0200

    add file table

---
+++ Data/Models/Files.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace API_PRO.Data.Models
+{
+    public class Files
+    {
+        [Key]
+        public int FileId { get; set; } 
+        public string? Filepath { get; set; }
+        [ForeignKey("subject")]
+        public int SubjectId { get; set; }
+
+        public virtual Subject subject { get; set; }    
+        [NotMapped]
+        public IFormFile file { get; set; }
+    }
+}
