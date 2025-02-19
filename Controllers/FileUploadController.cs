f14fb2ce12144a3782523fe16379ffa5c48e2486
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-19 23:18:42 +0200

    add post method to file contoller

--- Controllers/FileUploadController.cs
+++ Controllers/FileUploadController.cs
@@ -1,5 +1,12 @@
-﻿using API_PRO.Models;
+﻿using API_PRO.Data;
+using API_PRO.Data;
+using API_PRO.Data.Models;
+using API_PRO.Models;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
 using Microsoft.AspNetCore.Mvc;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace API_PRO.Controllers
 {
@@ -8,11 +15,35 @@ namespace API_PRO.Controllers
     [Route("api/[controller]")]
     public class FileUploadController : Controller
     {
-        
-        public FileUploadController()
+        private readonly IDataRepository<Files> _filerepo;
+        private readonly IHostingEnvironment _env;
+        private readonly IMapper _map;
+
+        public FileUploadController(IDataRepository<Files> filerepo, IHostingEnvironment env , IMapper map)
         {
-            
+            _filerepo = filerepo;
+            _env = env;
+            _map = map;
+
         }
+        [HttpPost("upload")]
+        public async Task<IActionResult> upload([FromForm] FileUploadDto filedto)
+        {
+            if (filedto.file == null || filedto.file.Length == 0)
+                return BadRequest("file is null");
+            var filefolder = Path.Combine(_env.WebRootPath ,"uploads");
+            if (!Directory.Exists(filefolder))
+            { 
+                Directory.CreateDirectory(filefolder);
+            }
+            var filepath = Path.Combine(filefolder,filedto.file.FileName);
+            using (var stream = new FileStream(filepath, FileMode.Create))
+            {
+                await filedto.file.CopyToAsync(stream);
+            }
+            return Ok();
+        }
+
         //[HttpPost("upload-dto")]
         //public async Task<IActionResult> UploadFileWithDto([FromForm] FileUploadDto dto)
         //{
