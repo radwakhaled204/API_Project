1d1e860a608f53deecb0a4fdd1e2753918b14128
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-19 21:25:44 +0200

    edit subcontroller

--- Controllers/SubjectController.cs
+++ Controllers/SubjectController.cs
@@ -3,6 +3,7 @@ using API_PRO.Data.Models;
 using API_PRO.Models;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace API_PRO.Controllers
 {
@@ -12,26 +13,28 @@ namespace API_PRO.Controllers
     {
         private readonly IDataRepository<Subject> _subjectRepository;
         private readonly IMapper _mapper;
+        private readonly IHostingEnvironment _env;
 
-        public SubjectController(IDataRepository<Subject> subjectRepository, IMapper mapper)
+        public SubjectController(IDataRepository<Subject> subjectRepository, IMapper mapper , IHostingEnvironment env)
         {
             _subjectRepository = subjectRepository;
             _mapper = mapper;
+            _env = env;
         }
 
-        // استرجاع جميع المواد كـ DTO
+      
         [HttpGet]
         public async Task<IActionResult> GetAllSubjects()
         {
             var subjects = await _subjectRepository.GetAllFun();
 
-            // تحويل الكيانات إلى DTO
+          
             var subjectDtos = _mapper.Map<IEnumerable<SubjectDto>>(subjects);
 
             return Ok(subjectDtos);
         }
 
-        // استرجاع مادة معينة كـ DTO
+      
         [HttpGet("{id}")]
         public async Task<IActionResult> GetSubjectById(int id)
         {
@@ -41,22 +44,22 @@ namespace API_PRO.Controllers
                 return NotFound();
             }
 
-            // تحويل الكائن Entity إلى DTO
+           
             var subjectDto = _mapper.Map<SubjectDto>(subject);
             return Ok(subjectDto);
         }
 
-        // إضافة مادة جديدة
+       
         [HttpPost]
         public async Task<IActionResult> AddSubject(SubjectDto subjectDto)
         {
-            var subject = _mapper.Map<Subject>(subjectDto); // تحويل DTO إلى Entity
+            var subject = _mapper.Map<Subject>(subjectDto); 
 
             await _subjectRepository.AddFun(subject);
             return CreatedAtAction(nameof(GetSubjectById), new { id = subject.SubjectId }, subjectDto);
         }
 
-        // تعديل اسم المادة
+        
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateSubject(int id, SubjectDto subjectDto)
         {
@@ -66,14 +69,14 @@ namespace API_PRO.Controllers
                 return NotFound();
             }
 
-            // تحويل الـ DTO إلى Entity
+         
             _mapper.Map(subjectDto, subject);
 
             await _subjectRepository.UpdateFun(subject);
             return NoContent();
         }
 
-        // حذف مادة
+       
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteSubject(int id)
         {
@@ -86,26 +89,7 @@ namespace API_PRO.Controllers
             await _subjectRepository.DeleteFun(id);
             return NoContent();
         }
-        //[HttpPost("upload-dto")]
-        //public async Task<IActionResult> UploadFileWithDto([FromForm] FileUploadDto dto)
-        //{
-        //    if (dto.File == null || dto.File.Length == 0)
-        //        return BadRequest("الملف غير موجود أو فارغ.");
-
-        //    var uploadsFolder = Path.Combine(_environment.WebRootPath, "uploads");
-
-        //    if (!Directory.Exists(uploadsFolder))
-        //        Directory.CreateDirectory(uploadsFolder);
-
-        //    var filePath = Path.Combine(uploadsFolder, dto.File.FileName);
-
-        //    using (var stream = new FileStream(filePath, FileMode.Create))
-        //    {
-        //        await dto.File.CopyToAsync(stream);
-        //    }
-
-        //    return Ok(new { Message = "تم الرفع بنجاح!", FileName = dto.File.FileName });
-        //}
+   
 
     }
 }
