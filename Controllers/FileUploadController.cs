c21c5a9d1ad5777fb16444e047c3a3bcf279d967
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-21 19:00:23 +0200

    fix errors

--- Controllers/FileUploadController.cs
+++ Controllers/FileUploadController.cs
@@ -1,12 +1,11 @@
 ﻿using API_PRO.Data;
-using API_PRO.Data;
 using API_PRO.Data.Models;
 using API_PRO.Models;
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 
 using Microsoft.AspNetCore.Mvc;
-using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+using IWebHostEnvironment = Microsoft.AspNetCore.Hosting.IWebHostEnvironment;
 
 namespace API_PRO.Controllers
 {
@@ -16,10 +15,10 @@ namespace API_PRO.Controllers
     public class FileUploadController : Controller
     {
         private readonly IDataRepository<Files> _filerepo;
-        private readonly IHostingEnvironment _env;
+        private readonly IWebHostEnvironment _env;
         private readonly IMapper _map;
 
-        public FileUploadController(IDataRepository<Files> filerepo, IHostingEnvironment env , IMapper map)
+        public FileUploadController(IDataRepository<Files> filerepo, IWebHostEnvironment env , IMapper map)
         {
             _filerepo = filerepo;
             _env = env;
@@ -27,22 +26,49 @@ namespace API_PRO.Controllers
 
         }
         [HttpPost("upload")]
-        public async Task<IActionResult> upload([FromForm] FileUploadDto filedto)
+        public async Task<IActionResult> Upload([FromForm] FileUploadDto filedto)
         {
             if (filedto.file == null || filedto.file.Length == 0)
-                return BadRequest("file is null");
-            var filefolder = Path.Combine(_env.WebRootPath ,"uploads");
+                return BadRequest("الملف غير موجود.");
+
+     
+            if (string.IsNullOrEmpty(_env.WebRootPath))
+            {
+                _env.WebRootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+            }
+
+            var filefolder = Path.Combine(_env.WebRootPath, "uploads");
+
             if (!Directory.Exists(filefolder))
-            { 
+            {
                 Directory.CreateDirectory(filefolder);
             }
-            var filepath = Path.Combine(filefolder,filedto.file.FileName);
+
+           
+            var uniqueFileName = $"{Guid.NewGuid()}_{filedto.file.FileName}";
+            var filepath = Path.Combine(filefolder, uniqueFileName);
+
+      
             using (var stream = new FileStream(filepath, FileMode.Create))
             {
                 await filedto.file.CopyToAsync(stream);
             }
-            return Ok();
+
+          
+            var fileRecord = new Files
+            {
+                FileName = filedto.file.FileName, 
+                FilePath = $"/uploads/{uniqueFileName}", 
+                SubjectId = filedto.subid           
+            };
+
+            await _filerepo.AddFun(fileRecord); 
+
+            return Ok(new { message = "تم تحميل الملف بنجاح.", path = fileRecord.FilePath });
         }
+
+
+
         [HttpGet("download/{FileName}")]
         public async Task<IActionResult> Download(string FileName)
         {
