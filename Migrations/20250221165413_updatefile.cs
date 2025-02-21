c1f933daa7b4476c124b6912ce3ed6fc8518341d
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-21 19:00:41 +0200

    update file table

---
+++ Migrations/20250221165413_updatefile.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_PRO.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatefile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Filepath",
+                table: "Files",
+                newName: "FilePath");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FileName",
+                table: "Files",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileName",
+                table: "Files");
+
+            migrationBuilder.RenameColumn(
+                name: "FilePath",
+                table: "Files",
+                newName: "Filepath");
+        }
+    }
+}
