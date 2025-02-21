aed492318c46c60d519b53a3c644897eff1cf8f0
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-21 17:25:38 +0200

    add download function

--- Controllers/FileUploadController.cs
+++ Controllers/FileUploadController.cs
@@ -43,26 +43,19 @@ namespace API_PRO.Controllers
             }
             return Ok();
         }
+        [HttpGet("download/{FileName}")]
+        public async Task<IActionResult> Download(string FileName)
+        {
+            var filepath = Path.Combine("wwwroot/uploads" , FileName);
+            if (!System.IO.File.Exists(filepath))
+            {
+                return NotFound();
+            }
+            var filebyte = await System.IO.File.ReadAllBytesAsync(filepath);
+            
+            return File(filebyte ,"application/pdf" , FileName);
+        }
 
-        //[HttpPost("upload-dto")]
-        //public async Task<IActionResult> UploadFileWithDto([FromForm] FileUploadDto dto)
-        //{
-        //    if (dto.file == null || dto.file.Length == 0)
-        //        return BadRequest("الملف غير موجود أو فارغ.");
 
-        //    var uploadsFolder = Path.Combine(_environment.WebRootPath, "uploads");
-
-        //    if (!Directory.Exists(uploadsFolder))
-        //        Directory.CreateDirectory(uploadsFolder);
-
-        //    var filePath = Path.Combine(uploadsFolder, dto.File.FileName);
-
-        //    using (var stream = new FileStream(filePath, FileMode.Create))
-        //    {
-        //        await dto.File.CopyToAsync(stream);
-        //    }
-
-        //    return Ok(new { Message = "تم الرفع بنجاح!", FileName = dto.File.FileName });
-        //}
     }
 }
