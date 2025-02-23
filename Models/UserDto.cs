1f991a803fda73d9f205afe754a1669a9ae60baa
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-23 16:06:20 +0200

    Update userdto

---
+++ Models/UserDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_PRO.Models
+{
+    public class UserDto
+    {
+        [Required(ErrorMessage = "Password is required")]
+        public required string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public required string Password { get; set; }
+
+        [EmailAddress(ErrorMessage = "Please enter valid email")]
+        public required string Email { get; set; }
+        //public string? phoneNumber { get; set; }
+    }
+}
