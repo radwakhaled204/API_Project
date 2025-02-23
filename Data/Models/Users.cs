4f69fe0f38a42acc9b3bfd7f2b50f877c54ae991
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-23 16:04:42 +0200

    add user model

--- Data/Models/Users.cs
+++ Data/Models/Users.cs
@@ -1,20 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
 namespace API_PRO.Data.Models
 {
+
     public class Users : IdentityUser
     {
-        [Key]
-        public int UserId { get; set; }
-
-        [Required]
-        public  string UserName { get; set; }
-        [Required]
-        public  string Password { get; set; }
-
-
-
     }
-
 }
