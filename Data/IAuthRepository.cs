fdecaa6f8023ec221e01f84b508fd3126c86cb07
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-23 16:05:52 +0200

    add IAuthRepository

---
+++ Data/IAuthRepository.cs
@@ -0,0 +1,13 @@
+﻿using API_PRO.Data.Models;
+
+namespace API_PRO.Data
+{
+    public interface IAuthRepository
+    {
+
+        Task<Users> Register(Users user, string password);
+        //Task<Users> Login(string email, string password);
+        //Task<Admin> LoginAdmin(string email, string password);
+        Task<bool> UserExist(string username);
+    }
+}
