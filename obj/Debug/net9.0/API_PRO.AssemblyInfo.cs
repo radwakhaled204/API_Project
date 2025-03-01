a6de0040c61bf3b45e6824af1b0683347514f22f
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-01 19:09:56 +0200

    update pro

--- obj/Debug/net9.0/API_PRO.AssemblyInfo.cs
+++ obj/Debug/net9.0/API_PRO.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_PRO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69f47b0ee0d381526010566bfd4f3f6ad6755596")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bdf3194bbdb9712d7e339ae290a053a96878f08")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_PRO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_PRO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
