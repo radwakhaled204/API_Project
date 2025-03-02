409b48364d41bc47c8c43c7e86d93099174daf37
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-03 00:29:51 +0200

    update pro

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.JsonPatch;
 
 
 namespace API_PRO.Controllers
-{//3
+{//12
     [Route("api/[controller]")]
     [ApiController]
     public class CategoriesController : Controller
