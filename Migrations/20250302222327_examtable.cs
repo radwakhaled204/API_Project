181ead400a317eb4ac20ff7ff185fa86ae7c90c5
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-03 00:24:19 +0200

    add new migration

---
+++ Migrations/20250302222327_examtable.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_PRO.Migrations
+{
+    /// <inheritdoc />
+    public partial class examtable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Exam",
+                columns: table => new
+                {
+                    ExamId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ExamName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    QuestionType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumQuestions = table.Column<int>(type: "int", nullable: false),
+                    NumCorrectQuestions = table.Column<int>(type: "int", nullable: true),
+                    NumWrongQuestions = table.Column<int>(type: "int", nullable: true),
+                    DifficultyLevel = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    McqQuestionsData = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TfQuestionsData = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TimeTaken = table.Column<TimeOnly>(type: "time", nullable: true),
+                    TotalScore = table.Column<int>(type: "int", nullable: true),
+                    XpCollected = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FileId = table.Column<int>(type: "int", nullable: true),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    SubjectId = table.Column<int>(type: "int", nullable: true),
+                    UserId1 = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exam", x => x.ExamId);
+                    table.ForeignKey(
+                        name: "FK_Exam_AspNetUsers_UserId1",
+                        column: x => x.UserId1,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Exam_Files_FileId",
+                        column: x => x.FileId,
+                        principalTable: "Files",
+                        principalColumn: "FileId");
+                    table.ForeignKey(
+                        name: "FK_Exam_Subject_SubjectId",
+                        column: x => x.SubjectId,
+                        principalTable: "Subject",
+                        principalColumn: "SubjectId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exam_FileId",
+                table: "Exam",
+                column: "FileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exam_SubjectId",
+                table: "Exam",
+                column: "SubjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exam_UserId1",
+                table: "Exam",
+                column: "UserId1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Exam");
+        }
+    }
+}
