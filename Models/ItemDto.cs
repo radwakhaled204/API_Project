8620b7608be79d8a1580f52f39f855948e3b4619
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-04 20:03:58 +0200

    update

---
+++ Models/ItemDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_PRO.Models
+{
+    public class ItemDto
+    {
+        [MaxLength(50)]
+        public string Name { get; set; }
+        public string? Notes { get; set; }
+        public double Price { get; set; }
+        public IFormFile? Image { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
