268bbce3919df365dbcddb4ff919a1802b648ff2
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-05 21:14:04 +0200

    add function

--- Data/IDataRepository.cs
+++ Data/IDataRepository.cs
@@ -2,7 +2,12 @@
 {
     public interface IDataRepository<T> where T : class
     {
-        
+        //public Task<List<ComplaintsDTO>> GetAll();
+        //public Task<ComplaintsDTO> GetById(int id);
+        //public Task<int> Create(ComplaintsDTO complaintDto);
+        //public Task<ComplaintsDTO> Update(int id, ComplaintsDTO complaintDto);
+        //public Task<int> Delete(int id);
+
         Task<IEnumerable<T>> GetAllFun();
         Task<T> GetByIdFun(int id);
         Task AddFun(T entity);
