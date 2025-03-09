066f7890d569512e4ac24f6dcef9311731cfa98f
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-09 22:40:51 +0200

    update

--- Program.cs
+++ Program.cs
@@ -62,8 +62,7 @@ if (string.IsNullOrEmpty(app.Environment.WebRootPath))
 {
     app.Environment.WebRootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
 }//==builder.WebHost.UseWebRoot("wwwroot");
-//1
-// 
+
 app.UseStaticFiles();
 
 app.UseHttpsRedirection();
