d4c39db77802880319ff75b8cdd3705568dfc0e9
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-11 22:27:19 +0200

    fix function

--- Controllers/ExamController.cs
+++ Controllers/ExamController.cs
@@ -96,51 +96,51 @@ namespace API_PRO.Controllers
                 await _examRepo.DeleteFun(id);
                 return Ok("Exam Deleted Successfully");
             }
-            //[HttpPatch("{id}/{subId}")]
-            //public async Task<IActionResult> MoveExamToAnotherSub(int subId, [FromRoute] int id)
-            //{
-            //    var exam = await _examRepo.GetByIdFun(id);
-            //    if (exam == null)
-            //    {
-            //        return NotFound($"Exam with ID {id} not found.");
-            //    }
+        [HttpPatch("{id}/{subId}")]
+        public async Task<IActionResult> MoveExamToAnotherSub(int subId, [FromRoute] int id)
+        {
+            var exam = await _examRepo.GetByIdFun(id);
+            if (exam == null)
+            {
+                return NotFound($"Exam with ID {id} not found.");
+            }
 
-            //    if (exam.SubjectId != null)
-            //    {
-            //        //var oldSubject = await _subjectRepo.GetByIdFun(exam.SubjectId);
-            //        if (oldSubject == null)
-            //        {
-            //            return NotFound($"Old Subject with ID {exam.SubjectId} not found.");
-            //        }
+            if (exam.SubjectId != null)
+            {
+                var oldSubject = await _subjectRepo.GetByIdFun(exam.SubjectId);
+                if (oldSubject == null)
+                {
+                    return NotFound($"Old Subject with ID {exam.SubjectId} not found.");
+                }
 
-            //        oldSubject.NumExams -= 1;
-            //        oldSubject.TotalQuestions -= exam.NumQuestions;
-            //        if (oldSubject.NumExams < 0 || oldSubject.TotalQuestions < 0)
-            //        {
-            //            return BadRequest("The number of exams or number of TotalQuestions in the old subject cannot be negative.");
-            //        }
-            //        await _subjectRepo.UpdateFun(oldSubject);
-            //    }
+                oldSubject.NumExams -= 1;
+                oldSubject.TotalQuestions -= exam.NumQuestions;
+                if (oldSubject.NumExams < 0 || oldSubject.TotalQuestions < 0)
+                {
+                    return BadRequest("The number of exams or number of TotalQuestions in the old subject cannot be negative.");
+                }
+                await _subjectRepo.UpdateFun(oldSubject);
+            }
 
-            //    var newSubject = await _subjectRepo.GetByIdFun(subId);
-            //    if (newSubject == null)
-            //    {
-            //        return NotFound($"New Subject with ID {subId} not found.");
-            //    }
+            var newSubject = await _subjectRepo.GetByIdFun(subId);
+            if (newSubject == null)
+            {
+                return NotFound($"New Subject with ID {subId} not found.");
+            }
 
-            //    newSubject.NumExams += 1;
-            //    newSubject.TotalQuestions += exam.NumQuestions;
-            //    await _subjectRepo.UpdateFun(newSubject);
+            newSubject.NumExams += 1;
+            newSubject.TotalQuestions += exam.NumQuestions;
+            await _subjectRepo.UpdateFun(newSubject);
 
-            //    exam.SubjectId = subId;
-            //    await _examRepo.UpdateFun(exam);
+            exam.SubjectId = subId;
+            await _examRepo.UpdateFun(exam);
 
-            //    return Ok("Exam moved successfully.");
-            //}
-
-
+            return Ok("Exam moved successfully.");
+        }
 
 
-       
+
+
+
     }
 }
