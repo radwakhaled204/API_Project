afc9cf460bf8dc1cfe94acc05688f48e113ea1bc
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-16 22:02:16 +0200

    Update pro

--- Models/UserDto.cs
+++ Models/UserDto.cs
@@ -4,13 +4,13 @@ namespace API_PRO.Models
 {
     public class UserDto
     {
-        [Required(ErrorMessage = "Password is required")]
+        [Required(ErrorMessage = "Password Is Required")]
         public required string Username { get; set; }
 
-        [Required(ErrorMessage = "Password is required")]
+        [Required(ErrorMessage = "Password Is Required")]
         public required string Password { get; set; }
 
-        [EmailAddress(ErrorMessage = "Please enter valid email")]
+        [EmailAddress(ErrorMessage = "Please Enter Valid Email")]
         public required string Email { get; set; }
         //public string? phoneNumber { get; set; }
     }
