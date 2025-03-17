43bdb1f8322cdcc9c599c555b18247bf09559812
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-17 22:57:18 +0200

    Update Pro

--- Models/ItemDto.cs
+++ Models/ItemDto.cs
@@ -5,7 +5,7 @@ namespace API_PRO.Models
     public class ItemDto
     {
         [MaxLength(50)]
-        [Required(ErrorMessage = "Enter The Name")]
+        [Required(ErrorMessage = "Please Enter The Name")]
         public string Name { get; set; }
         public string? Notes { get; set; }
         public double Price { get; set; }
